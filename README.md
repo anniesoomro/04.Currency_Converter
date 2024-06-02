fb7b7684e430902e9c1bcfb375aafcba455f2737
anniesoomro <162474299+anniesoomro@users.noreply.github.com>
2024-06-02 16:15:18 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 04.Currency_Converter" 
---
+++ index.ts
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+let C_value: {[key: string]: number} = {
+    PKR: 278.40,
+    UAE: 3.67,
+    USD: 1,
+    INR: 3.33, 
+    Chinese_Yuan: 38.47,
+    Egyptian_Pound: 5.87,
+    Euro: 299.40,
+    Turkish_Lira: 8.59
+}
+
+let ans = await inquirer.prompt([{
+    type: "list",
+    name: "from",
+    message: "\nConverting From\n",
+    choices: ["PKR", "UAE", "USD", "INR", "Chinese_Yuan", "Egyptian_Pound", "Euro", "Turkish_Lira"]
+},
+{
+    type: "list",
+    name: "to",
+    message: "\nConverting to\n",
+    choices: ["PKR", "UAE", "USD", "INR", "Chinese_Yuan", "Egyptian_Pound", "Euro", "Turkish_Lira"]
+},
+{
+    type: "number",
+    name: "amount",
+    message: "\nYour amount to convert\n"
+}
+
+])
+
+
+console.log(chalk.bgGray.bold(C_value[ans.to] / C_value[ans.from] * ans.amount));
+
+
+
+
+
+
+
+
+
+
